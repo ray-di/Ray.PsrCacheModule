ce18f5c4013a63b0839f7954a849bc74023b799e
koriym <akihito.koriyama@gmail.com>
2021-06-21 14:00:18 +0900

    Add missing @Qualifier annotation

--- src/Annotation/Shared.php
+++ src/Annotation/Shared.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace Ray\PsrCacheModule\Annotation;
 
 use Attribute;
+use Ray\Di\Di\Qualifier;
 
-/** @Annotation */
-#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_METHOD)]
+/*
+ * @Annotation
+ * @Qualifier
+ */
+#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_METHOD), Qualifier]
 final class Shared
 {
 }
