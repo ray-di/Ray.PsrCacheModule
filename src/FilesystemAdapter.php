addc34d907b4b5ac6347eaf9d9cf9279770553d0
koriym <akihito.koriyama@gmail.com>
2021-06-26 15:45:50 +0900

    Call __construct on wakeup

--- src/FilesystemAdapter.php
+++ src/FilesystemAdapter.php
@@ -6,21 +6,30 @@ namespace Ray\PsrCacheModule;
 
 use Serializable;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter as OriginAdapter;
+use Symfony\Component\Cache\Marshaller\MarshallerInterface;
 
-use function get_object_vars;
+use function call_user_func_array;
+use function func_get_args;
 use function serialize;
 use function unserialize;
 
 class FilesystemAdapter extends OriginAdapter implements Serializable
 {
+    /** @var array<int, mixed> */
+    private $args;
+
+    public function __construct(string $namespace = '', int $defaultLifetime = 0, ?string $directory = null, ?MarshallerInterface $marshaller = null)
+    {
+        $this->args = func_get_args();
+        parent::__construct($namespace, $defaultLifetime, $directory, $marshaller);
+    }
+
     /**
      * @inheritDoc
      */
     public function serialize()
     {
-        $data = get_object_vars($this);
-
-        return serialize($data);
+        return serialize($this->args);
     }
 
     /**
@@ -28,10 +37,6 @@ class FilesystemAdapter extends OriginAdapter implements Serializable
      */
     public function unserialize($data)
     {
-        /** @var array<string, scalar|object> $data */
-        $data = unserialize($data);
-        foreach ($data as $name => $value) {
-            $this->$name = $value;
-        }
+        call_user_func_array([$this, '__construct'], unserialize($data));
     }
 }
