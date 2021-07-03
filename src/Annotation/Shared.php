9a47cedf26671a81a5ce29e10eb5686c4f78f505
koriym <akihito.koriyama@gmail.com>
2021-07-03 16:38:09 +0900

    PHP7 compatibility for annotations

--- src/Annotation/Shared.php
+++ src/Annotation/Shared.php
@@ -5,13 +5,22 @@ declare(strict_types=1);
 namespace Ray\PsrCacheModule\Annotation;
 
 use Attribute;
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 use Ray\Di\Di\Qualifier;
 
 /**
  * @Annotation
  * @Qualifier
+ * @NamedArgumentConstructor
  */
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_METHOD), Qualifier]
 final class Shared
 {
+    /** @var string */
+    public $value;
+
+    public function __construct(string $value = 'cache')
+    {
+        $this->value = $value;
+    }
 }
