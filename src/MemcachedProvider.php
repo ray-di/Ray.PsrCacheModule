e58e2e0217d4e6a02cbca5d23ba1a8793228de39
koriym <akihito.koriyama@gmail.com>
2021-07-04 17:30:13 +0900

    Add memcache module

---
+++ src/MemcachedProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ray\PsrCacheModule;
+
+use Memcached;
+use Ray\Di\ProviderInterface;
+use Ray\PsrCacheModule\Annotation\MemcacheConfig;
+
+class MemcachedProvider implements ProviderInterface
+{
+    /**
+     * memcached server list
+     *
+     * @var array<array<string>>
+     */
+    private $servers;
+
+    /**
+     * @param array<array<string>> $servers
+     *
+     * @MemcacheConfig("servers")
+     * @see https://www.php.net/manual/en/memcached.addservers.php
+     */
+    #[MemcacheConfig('servers')]
+    public function __construct(array $servers)
+    {
+        $this->servers = $servers;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get()
+    {
+        $memcache = new Memcached();
+        $memcache->addServers($this->servers);
+
+        return $memcache;
+    }
+}
---
+++ src/Psr6MemcachedModule.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ray\PsrCacheModule;
+
+use Memcached;
+use Psr\Cache\CacheItemPoolInterface;
+use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
+use Ray\PsrCacheModule\Annotation\CacheNamespace;
+use Ray\PsrCacheModule\Annotation\Local;
+use Ray\PsrCacheModule\Annotation\MemcacheConfig;
+use Ray\PsrCacheModule\Annotation\Shared;
+use Symfony\Component\Cache\Adapter\MemcachedAdapter;
+
+use function array_map;
+use function explode;
+
+final class Psr6MemcachedModule extends AbstractModule
+{
+    /** @var list<list<string>> */
+    private $servers;
+
+    public function __construct(string $servers, ?AbstractModule $module = null)
+    {
+        $this->servers = array_map(static function ($serverString) {
+            return explode(':', $serverString);
+        }, explode(',', $servers));
+        parent::__construct($module);
+    }
+
+    protected function configure(): void
+    {
+        $this->bind(CacheItemPoolInterface::class)->annotatedWith(Local::class)->toProvider(LocalCacheProvider::class)->in(Scope::SINGLETON);
+        $this->bind(CacheItemPoolInterface::class)->annotatedWith(Shared::class)->toConstructor(MemcachedAdapter::class, [
+            'namespace' => CacheNamespace::class,
+        ])->in(Scope::SINGLETON);
+        $this->bind()->annotatedWith(MemcacheConfig::class)->toInstance($this->servers);
+        $this->bind(Memcached::class)->toProvider(MemcachedProvider::class);
+    }
+}
