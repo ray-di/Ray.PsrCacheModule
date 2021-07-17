be11d137db3e926da36b0414048985600665523a
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-07-17 15:29:31 +0900

    Add serializable PhpFileAdapter

---
+++ tests/PhpFileAdapterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ray\PsrCacheModule;
+
+use PHPUnit\Framework\TestCase;
+
+use function serialize;
+use function unserialize;
+
+class PhpFileAdapterTest extends TestCase
+{
+    public function testSerialize(): string
+    {
+        $string = serialize(new PhpFileAdapter());
+        $this->assertIsString($string);
+
+        return $string;
+    }
+
+    /**
+     * @depends testSerialize
+     */
+    public function testUnserialize(string $string): void
+    {
+        $this->assertInstanceOf(PhpFileAdapter::class, unserialize($string));
+    }
+}
