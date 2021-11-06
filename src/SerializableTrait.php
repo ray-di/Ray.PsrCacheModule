4ed79028009cffa70c58aa30a373ca22e123dd7d
koriym <akihito.koriyama@gmail.com>
2021-11-06 22:28:29 +0900

    Revert deprecated serializable cache adapter
    Required as local cache

---
+++ src/SerializableTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ray\PsrCacheModule;
+
+use function call_user_func_array;
+
+trait SerializableTrait
+{
+    use Php73BcSerializableTrait;
+
+    /** @var array<mixed> */
+    private $args;
+
+    /**
+     * @return array<mixed>
+     */
+    public function __serialize(): array
+    {
+        return $this->args;
+    }
+
+    /**
+     * @param array<mixed> $data
+     */
+    public function __unserialize(array $data): void
+    {
+        call_user_func_array([$this, '__construct'], $data); // @phpstan-ignore-line
+    }
+}
