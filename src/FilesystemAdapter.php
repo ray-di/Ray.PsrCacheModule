07c029d338c22f7f084a190c1aba61ddecb5a3bd
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-07-28 08:34:00 +0900

    Create cachenamespace and cachrDir test for FileSystemAdapter

--- src/FilesystemAdapter.php
+++ src/FilesystemAdapter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ray\PsrCacheModule;
 
+use Ray\PsrCacheModule\Annotation\CacheDir;
 use Ray\PsrCacheModule\Annotation\CacheNamespace;
 use Serializable;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter as OriginAdapter;
@@ -17,8 +18,10 @@ class FilesystemAdapter extends OriginAdapter implements Serializable
 
     /**
      * @CacheNamespace("namespace")
+     * @CacheDir("directory")
      */
     #[CacheNamespace('namespace')]
+    #[CacheDir('directory')]
     public function __construct(string $namespace = '', int $defaultLifetime = 0, ?string $directory = null, ?MarshallerInterface $marshaller = null)
     {
         $this->args = func_get_args();
