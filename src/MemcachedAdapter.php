3cfd2d651b35cec50e3683129e5a45f1eadc8a0b
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-05-14 01:35:22 +0900

    fixup! Refactor code and add serialization test

--- src/MemcachedAdapter.php
+++ src/MemcachedAdapter.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Ray\PsrCacheModule;
 
-use Memcached;
-use Ray\Di\ProviderInterface;
-use Ray\PsrCacheModule\Annotation\CacheNamespace;
 use Serializable;
 use Symfony\Component\Cache\Adapter\MemcachedAdapter as OriginAdapter;
 use Symfony\Component\Cache\Marshaller\MarshallerInterface;
@@ -18,16 +15,10 @@ class MemcachedAdapter extends OriginAdapter implements Serializable
 {
     use SerializableTrait;
 
-    /**
-     * @param ProviderInterface<Memcached> $clientProvider
-     *
-     * @CacheNamespace("namespace")
-     */
-    #[CacheNamespace('namespace')]
-    public function __construct(MemcachedProvider $clientProvider, string $namespace = '', int $defaultLifetime = 0, ?MarshallerInterface $marshaller = null)
+    public function __construct(MemcachedProvider $provider, string $namespace = '', int $defaultLifetime = 0, ?MarshallerInterface $marshaller = null)
     {
         $this->args = func_get_args();
 
-        parent::__construct($clientProvider->get(), $namespace, $defaultLifetime, $marshaller);
+        parent::__construct($provider->get(), $namespace, $defaultLifetime, $marshaller);
     }
 }
